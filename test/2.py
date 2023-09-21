9a7ee1784bb06d9fd990bd6a93e759293f1fac70
Stefan Caras <stefan.caras@gmail.com>
2023-09-21 22:18:38 +0300

    stuff

--- test/2.py
+++ test/2.py
@@ -11,4 +11,12 @@ def majority(the_list):
     return ', '.join(new_list)
 
 
-print(majority([3, 2, 3]))
+def majority2(the_list):
+    my_dict = {}
+    for el in the_list:
+        my_dict[el] = (my_dict[el] + 1 if el in my_dict else 1)
+    result = [str(el[0]) for el in list(filter(lambda x: x[1] == max(my_dict.values()), my_dict.items()))]
+    return ', '.join(result)
+
+
+print(majority([3, 2, 2, 33]))
